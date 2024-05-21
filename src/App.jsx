54f3559a89067cebdbde7368a144abf7d0f58ac3
Refat Jamil <rifatjamil542gmail.com>
2024-05-21 22:30:54 +0600

    add cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from './components/Projects';
-import exampleImage from './images/img.png';
 
 function App() {
   const [activeTab, setActiveTab] = useState('about');
@@ -20,20 +18,16 @@ function App() {
         <div className="col-md-4"></div>
         <div className="col-md-4">
         {/* Social Media Icons */}
-        <div className="social-icons pb-2">
-            <a href="https://www.linkedin.com/in/refat-jamil/"><FontAwesomeIcon icon={faLinkedin} className="mr-2 text-dark" /></a>
-            <a href="https://github.com/refat-jamil"><FontAwesomeIcon icon={faGithub} className="mr-2 text-dark" /></a>
-          </div>
+        <div className="text-center pb-5">
 
-          <div className="text-center">
-            <img src={exampleImage} alt="Description of the image" style={{ maxWidth: '250px', height: 'auto' }} />
-            <h1 className='text-center my-2'>Refat Jamil</h1>
-          </div>
-          <div className="text-center mb-5 pb-3 mt-5">
             <button className={`rounded-pill btn btn-outline-dark px-3 mx-2 ${activeTab === 'about' ? 'active' : ''}`} onClick={() => handleTabClick('about')}>About</button>
             <button className={`rounded-pill btn btn-outline-dark px-3 mx-2 ${activeTab === 'skill' ? 'active' : ''}`} onClick={() => handleTabClick('skill')}>Skills</button>
             <button className={`rounded-pill btn btn-outline-dark px-3 mx-2 ${activeTab === 'projects' ? 'active' : ''}`} onClick={() => handleTabClick('projects')}>Projects</button>
+
           </div>
+        
+
+
           <div className="profile">
             {activeTab === 'about' && <About />}
             {activeTab === 'skill' && <Skills />}
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,51 @@
 import React from 'react';
+import exampleImage from '../images/img.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import cv from '../cv/cv.pdf';
 
 const About = () => {
+  const downloadCV = () => {
+    // Replace 'your-cv.pdf' with the actual path to your CV file
+    const cvUrl = cv;
+    window.open(cvUrl, '_blank');
+  };
+
   return (
-    <div className="pb-md-5 px-md-3 pt-md-4">
-      <p className="h3 mb-3">Hello! 👋😃 I'm Refat</p>
-      <p className="mb-4">I have completed my Diploma in Computer Technology at Patuakhali Polytechnic Institute. I'm passionate about technology and constantly seek opportunities to learn and grow.</p>
-      
-      <div className="mb-4">
-        <h4 className="mb-2">I worked on ...</h4>
-        <p className="mb-0">Web development, where I specialize in building web applications using Django and creating RESTful APIs with Django Rest Framework. I enjoy working on personal projects to level up my skills and explore new technologies.</p>
-      </div>
-      
-      <div className="mb-4">
-        <h4 className="mb-2">I am currently learning ...</h4>
-        <p className="mb-0">Myself in the field of data analytics, with a particular focus on SQL (Structured Query Language). Through self-study and online courses, I am acquiring proficiency in querying databases, manipulating data, and performing complex analyses.</p>
-      </div>
+    <>
+      <div className="pb-md-5 px-md-3 pt-md-4">
+        <div className="d-flex justify-content-between align-items-center pb-2">
+          <div className="social-icons">
+            <a href="https://www.linkedin.com/in/refat-jamil/" className="mr-2 text-dark">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a href="https://github.com/refat-jamil" className="mr-2 text-dark">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </div>
+          
+          {/* Download CV Button */}
+          <button className="btn btn-dark" onClick={downloadCV}>Download CV</button>
+        </div>
 
+        <div className="text-center mb-5 pb-3">
+          <img src={exampleImage} alt="Description of the image" style={{ maxWidth: '250px', height: 'auto' }} />
+        </div>
 
-    </div>
+        <p className="h3 mb-3">Hello! 👋😃 I'm Refat</p>
+        <p className="mb-4">I have completed my Diploma in Computer Technology at Patuakhali Polytechnic Institute. I'm passionate about technology and constantly seek opportunities to learn and grow.</p>
+
+        <div className="mb-4">
+          <h4 className="mb-2">I worked on ...</h4>
+          <p className="mb-0">Web development, where I specialize in building web applications using Django and creating RESTful APIs with Django Rest Framework. I enjoy working on personal projects to level up my skills and explore new technologies.</p>
+        </div>
+
+        <div className="mb-4">
+          <h4 className="mb-2">I am currently learning ...</h4>
+          <p className="mb-0">Myself in the field of data analytics, with a particular focus on SQL (Structured Query Language). Through self-study and online courses, I am acquiring proficiency in querying databases, manipulating data, and performing complex analyses.</p>
+        </div>
+      </div>
+    </>
   );
 }
 
